6e3876e90e5455c71fe19a9bce8a9c0fa34539fa
Tespoirs <tuyentuyem55555@gmail.com>
2024-08-30 14:45:54 +0700

    Mô tả ngắn gọn về các thay đổi

---
+++ src/services/api/pondTypeRequest.js
@@ -0,0 +1,23 @@
+import axiosClient from "./axiosClient"
+
+const DashboardRequestApi = {
+    pondTypeRequest: {
+        getPondTypeRequest: async () => await axiosClient.get("/PondType?pageSize=200&pageNumber=1"),
+
+        createPondTypeRequest: async (data) => await axiosClient.post("/PondType", data),
+
+        deletePondTypeRequest: async (id) => await axiosClient.delete(`/PondType?PondTypeId=${id}`),
+    },
+    PondRequest: {
+        getPondRequest: async () => await axiosClient.get("/Pond?pageSize=200&pageNumber=1"),
+        getPondRequestById: async (id) => await axiosClient.get(`/Pond?search=${id}&pageSize=200&pageNumber=1`),
+
+        createPondRequest: async (data) => await axiosClient.post("/Pond", data),
+
+        deletePondRequest: async (id) => await axiosClient.delete(`Pond?PondId=${id}`),
+    }
+
+}
+
+
+export default DashboardRequestApi;
